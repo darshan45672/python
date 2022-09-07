59a1974552bf1a3dcda45fd738eea4fb02f0f63a
Darshan bhandary <109609462+darshan45672@users.noreply.github.com>
2022-09-07 12:40:50 +0530

    Add files via upload
    using this program one can determine wether given integer is +ve or _ve

---
+++ to determine a given integer is +ve or -ve.py
@@ -0,0 +1,7 @@
+num=float(input('enter your integer number: '))
+if num==0:
+    print('integer is equal to zero!')
+elif num>0:
+    print('entered integer is positive!')
+else:
+    print('entered integer is negeative!')

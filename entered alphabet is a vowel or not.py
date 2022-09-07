8a896256f225036d461fd2d76686fb6979904cfa
Darshan bhandary <109609462+darshan45672@users.noreply.github.com>
2022-09-07 12:35:00 +0530

    Add files via upload

---
+++ entered alphabet is a vowel or not.py
@@ -0,0 +1,6 @@
+ch=input('enter any alphabet: ')
+if ch.upper()=="A" or ch.upper()=="E" or ch.upper()=="I" or ch.upper()=="O" or ch.upper()=="U":
+    print('enetered alphabet is a vowel!')
+else:
+    print('entered alphabet is not a vowel!')
+print('thank you!')

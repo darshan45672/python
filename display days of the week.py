4483283ccf6d49428b8fb69f8da32848a863e3ca
Darshan bhandary <109609462+darshan45672@users.noreply.github.com>
2022-09-12 19:04:33 +0530

    Add files via upload

---
+++ display days of the week.py
@@ -0,0 +1,19 @@
+n=int(input('Enter a number between 1-7: '))
+if n==1:
+    print('Monday')
+elif n==2:
+    print('Tuesday')
+elif n==3:
+    print('Wednesday')
+elif n==4:
+    print('Thursday')
+elif n==5:
+    print('Friday')
+elif n==6:
+    print('Saturday')
+elif n==7:
+    print('Sunday')
+else:
+    print('SORRY!')
+    print('*    Entered number is between 1-7!  *')
+print('Thank You')
